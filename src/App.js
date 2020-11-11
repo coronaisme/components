853bb6126dc148b68172741dba091dd01546c8f0
coronaisme <coronaisme@gmail.com>
2020-11-11 14:54:40 -0800

    functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ResultComponent from "./components/ResultComponent";
+import KeypadComponent from "./components/KeypadComponent";
 
 function App() {
+  const [result, setResult] = useState(" ");
+
+  const calculate = () => {
+    try {
+      setResult({
+        result: (eval(result) || "") + "",
+      });
+    } catch (e) {
+      setResult({
+        result: "error",
+      });
+    }
+  };
+
+  const reset = () => {
+    setResult({
+      result: "",
+    });
+  };
+
+  const backspace = () => {
+    setResult({
+      result: result.slice(0, -1),
+    });
+  };
+
+  
+
   return (
-    <div className="App">
-      <h1>Basic Calculator</h1>
-      <form>
-        <input type="text" id="result" readOnly />
-        <input type="text" id="num" placeholder="Enter a Number" />
-        <button>Add</button>
-      </form>
+    <div className="Calculator">
+      <h1>Calculate!</h1>
+      <ResultComponent result={result} />
+      <KeypadComponent onClick={onClick} />
     </div>
   );
 }
